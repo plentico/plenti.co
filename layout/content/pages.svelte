5a7047334795a6f628d9fab880c4e3aad99ed498
jimafisk <jimafisk@gmail.com>
2020-06-19 15:03:34 -0400

    Add theme images.

--- layout/content/pages.svelte
+++ layout/content/pages.svelte
@@ -1,15 +1,33 @@
 <script>
-  export let title, description;
+  export let title, description, themes;
 </script>
 
 <div class="container">
   <h1>{title}</h1>
 
+  {#if description}
   <div>
     {#each description as paragraph}
       <p>{@html paragraph}</p>
     {/each}
   </div>
+  {/if}
+
+  {#if themes}
+    {#each themes as theme}
+      <img src="/assets/themes/{theme}" />
+    {/each}
+  {/if}
 
   <p><a href="/">Back home</a></p>
 </div>
+
+<style>
+
+  h1 {
+    margin-top: 40px;
+  }
+  img {
+		box-shadow: 0 10px 36px 0 rgba(0,0,0,.16), 0 0 0 1px rgba(0,0,0,.06);
+  }
+</style>
