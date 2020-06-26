be67b5ad5140c65191040b2baa923eaf5c62851e
jimafisk <jim.fisk@jantcu.com>
2020-06-26 01:40:54 +0000

    Deploying to gh-pages from  @ 5e4d1628bac3b2e7a33ddd2da3f01b462acc5c7f 🚀

--- spa/global/nav.js
+++ spa/global/nav.js
@@ -17,13 +17,18 @@ function create_fragment(ctx) {
 		c() {
 			nav = element("nav");
 
-			nav.innerHTML = `<div class="container svelte-edz9n5"><span id="brand" class="svelte-edz9n5"><a href="/" class="svelte-edz9n5"><img alt="planarian" src="/assets/planarian.svg" class="svelte-edz9n5">Plenti</a></span> 
-    <a class="nav-link svelte-edz9n5" href="/docs">Docs</a> 
-    <a class="nav-link svelte-edz9n5" href="/themes">Themes</a> 
-    <a class="nav-link button svelte-edz9n5" href="/download">Download</a> 
-    <a class="nav-link svelte-edz9n5" href="https://github.com/plentico/plenti"><img src="/assets/social/github.svg" class="svelte-edz9n5"></a></div>`;
+			nav.innerHTML = `<div class="container svelte-1hks6yg"><span id="brand" class="svelte-1hks6yg"><a href="/" class="svelte-1hks6yg"><img alt="planarian" src="/assets/planarian.svg" class="svelte-1hks6yg">Plenti</a></span> 
+    <a class="nav-link svelte-1hks6yg" href="/docs">Docs</a> 
+    <a class="nav-link svelte-1hks6yg" href="/themes">Themes</a> 
+    <a class="nav-link button svelte-1hks6yg" href="/download">Download</a> 
+    <a class="nav-link svelte-1hks6yg" href="https://github.com/plentico/plenti"><img src="/assets/social/github.svg" class="svelte-1hks6yg"></a> 
+    <input type="checkbox" id="toggle" class="hidden svelte-1hks6yg"> 
+    <label for="toggle" id="hamburger" class="svelte-1hks6yg"><span class="svelte-1hks6yg"></span> 
+      <span class="svelte-1hks6yg"></span> 
+      <span class="svelte-1hks6yg"></span> 
+      <span class="svelte-1hks6yg"></span></label></div>`;
 
-			attr(nav, "class", "svelte-edz9n5");
+			attr(nav, "class", "svelte-1hks6yg");
 		},
 		m(target, anchor) {
 			insert(target, nav, anchor);
