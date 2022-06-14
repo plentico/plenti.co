d7dfa5a4539cbb2b3c2322a997d5a2d4d5bc1ce1
jimafisk <jim.fisk@jantcu.com>
2022-06-14 03:49:14 +0000

    Deploying to gh-pages from  @ 92e1dbb012652e17586063633042e73645288166 🚀

--- spa/ejected/cms/get_assets.js
+++
@@ -1,9 +0,0 @@
-export const getAssets = dir => {
-  return fetch(dir, {cache: "no-store"})
-    .then(response => response.text())
-    .then(data => {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(data, 'text/html');
-        return doc.querySelectorAll('a');
-    });
-}
--- spa/ejected/env.js
+++ spa/ejected/env.js
@@ -1,1 +1,1 @@
-export let env = { local: false, baseurl: '', cms: { repo: '', redirectUrl: '', appId: '', branch: '' } };
+export let env={local:!1,baseurl:"",cms:{repo:"",redirectUrl:"",appId:"",branch:""}}
