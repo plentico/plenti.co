acbb02337ba99d6f1558552552ae1902f1bc3aa6
jimafisk <jimafisk@gmail.com>
2022-06-13 23:00:25 -0400

    Fix arrow icon for mobile.

--- layouts/content/themes.svelte
+++ layouts/content/themes.svelte
@@ -5,7 +5,14 @@
 </script>
 
 <div class="container">
-    <a href="/themes" class="back"><span class="arrow">🠔</span>Back to themes list</a>
+    <a href="/themes" class="back"><span class="arrow">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="25" height="25" viewBox="0 0 24 24" stroke-width="2" stroke="#22a6ed" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <line x1="5" y1="12" x2="19" y2="12" />
+        <line x1="5" y1="12" x2="9" y2="16" />
+        <line x1="5" y1="12" x2="9" y2="8" />
+    </svg>
+    </span>Back to themes list</a>
 
     <h1>{name}</h1>
 
@@ -40,8 +47,8 @@
     }
     .arrow {
         position: absolute;
-        left: -20px;
-        top: 2px;
+        left: -24px;
+        top: -4px;
     }
     a {
         font-weight: bold;
