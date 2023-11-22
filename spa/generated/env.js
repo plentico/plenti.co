57606c9c6a8dc074b7582d1ed52e4c90348aa55d
jimafisk <jim.fisk@jantcu.com>
2023-11-22 22:11:22 +0000

    Deploying to gh-pages from  @ 370298c51d52ac169fee340a40b0a1f144ecce69 🚀

--- spa/generated/env.js
+++ spa/generated/env.js
@@ -1,1 +1,1 @@
-export let env={local:!1,baseurl:"",entrypoint:"global/html.svelte",cms:{repo:"",redirectUrl:"",appId:"",branch:""}}
+export let env={local:!1,baseurl:"",entrypointHTML:"global/html.svelte",entrypointJS:"spa",cms:{repo:"",redirectUrl:"",appId:"",branch:""}}
